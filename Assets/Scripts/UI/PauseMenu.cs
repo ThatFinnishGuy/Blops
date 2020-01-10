958fc748f86dfdf25f9ce65215a81db9891e3609
ThatFinnishGuy <ilari.saarenpaa@gmail.com>
2020-01-10 19:43:41 +0200

    Initial commit

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Pause();
+    }
+
+    void OnDisable()
+    {
+        Resume();
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+}
